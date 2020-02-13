ed6dade5d7f7f7adf6b6e5d33b8ee52cbd3718f4
moripp <msa.m16a.i212z4a@gmail.com>
2020-02-13 15:45:14 +0900

    サインアップ、ログイン機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+  # サインアップの時にnameを許可する
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
+  # サインインしてない時はログイン画面へ遷移させる
+  def move_to_Log_in
+    redirect_to new_user_session_path unless user_signed_in?
+  end
+  
 end
