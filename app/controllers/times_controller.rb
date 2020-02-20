e5eabfdb971627d1297ccb52d58afc5d3227a38a
moripp <msa.m16a.i212z4a@gmail.com>
2020-02-20 18:27:24 +0900

    Update times_controller.rb

--- app/controllers/times_controller.rb
+++ app/controllers/times_controller.rb
@@ -1,4 +1,16 @@
 class TimesController < ApplicationController
   def index
+    @users = User.all
+  end
+
+  def new
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
+
+  def create
+    binding.pry
   end
 end
