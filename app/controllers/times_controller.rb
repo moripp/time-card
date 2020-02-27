07d59b2d929bd80233fa92fd981116d61d90a9f0
moripp <msa.m16a.i212z4a@gmail.com>
2020-02-27 17:22:59 +0900

    ユーザー検索

--- app/controllers/times_controller.rb
+++ app/controllers/times_controller.rb
@@ -1,5 +1,8 @@
 class TimesController < ApplicationController
   def index
+    return nil if params[:keyword] == ""
+    @users = User.where(['name LIKE ?', "%#{params[:keyword]}%"]).limit(10)
+
     respond_to do |format|
       format.html
       format.json
